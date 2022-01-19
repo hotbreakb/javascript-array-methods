e8be4faedba1730fc38fb6f34752450b12087729
hotbreakb <areyhappy12@gmail.com>
2022-01-20 08:40:22 +0900

    delete annotation

--- src/methods.js
+++ src/methods.js
@@ -13,24 +13,24 @@ const filteredItems = items.filter((item) => {
     return item.price <= 100;
 });
 
-// console.log(items); // not changing
-// console.log(filteredItems);
+console.log(items); // not changing
+console.log(filteredItems);
 
 // (2) map - make array containing only key or value
 const itemsNames = items.map((item) => {
     return item.name;
 });
 
-// console.log(items); // not changing
-// console.log(itemsNames); // ["Bike", "TV", "Album", "Book", "Phone", "Computer", "Keyboard"]
+console.log(items); // not changing
+console.log(itemsNames); // ["Bike", "TV", "Album", "Book", "Phone", "Computer", "Keyboard"]
 
 // (3) find - first item in the array which true in func
 const foundsNames = items.find((item) => {
     return item.price <= 100;
 });
 
-// console.log(items); // not changing
-// console.log(foundsNames); // {name: "Bike", price: 100}
+console.log(items); // not changing
+console.log(foundsNames); // {name: "Bike", price: 100}
 
 // (4) forEach
 items.forEach(item => {
@@ -42,23 +42,24 @@ let hasInexpensiveItems = items.some(item => {
     return item.price <= 100;
 });
 
-// console.log(hasInexpensiveItems); // true
+console.log(hasInexpensiveItems); // true
 
 // (6) every
 hasInexpensiveItems = items.every(item => {
     return item.price <= 100;
 });
 
-// console.log(hasInexpensiveItems); // false
+console.log(hasInexpensiveItems); // false
 
 // (7) reduce - 누적값, 현재값, 시작값(0)
 const total = items.reduce((currentTotal, item) => {
     return item.price + currentTotal;
 }, 0);
 
-// console.log(total); // 1840
+console.log(total); // 1840
 
 // (8)
 const numbers = [1, 2, 3, 4, 5];
 const includeTwo = numbers.includes(2);
+
 console.log(includeTwo); // true
